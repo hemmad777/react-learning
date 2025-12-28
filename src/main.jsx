5c91224c2c93a9bbab6c23e98c4a03ba084021da
Umar Hemmad <hemmad777@gmail.com>
2025-12-28 18:34:30 +0530

    Created a text box if type inside that thne show in another p tag the current value

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,29 @@
 
 import { createRoot } from "react-dom/client";
+import { useState } from "react";
+
 
 function MyForm(){
+
+  const [name,setName]=useState("");
+
+  const handleChange=(e)=> {
+    setName(e.target.value);  
+  }
+
   return(
     <>
     <form action="">
-      <label htmlFor="name">Enter your name</label>
-      <input type="text" />
+      <label htmlFor="name">Enter your name
+        <input type="text" 
+        value={name}
+        onChange={handleChange}
+        />
+
+      </label>
+      <p>Current value {name}</p>
+      
+  
     </form>
     </>
   )
